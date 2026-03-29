e46384198fbc7761890ea16491e8df37c109706f
KurimisettyTarun <169027569+KurimisettyTarun@users.noreply.github.com>
2026-03-29 15:23:13 +0530

    Time: 1 ms (100%), Space: 44.8 MB (11.22%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,17 +1,5 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-        // char[] res=s1.toCharArray();
-        // int temp=0;
-        // while(temp<2)
-        // {
-        //     char c = res[temp];
-        //     res[temp] = res[temp+2];
-        //     res[temp+2] = c;
-        //     temp++;
-        // }
-        // return String.valueOf(res).equals(s2);
-
-
             return ((s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
                 (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0)))
             &&
