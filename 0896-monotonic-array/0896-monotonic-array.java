6e401cede01b95fc84c25628e348d45be02bdbfa
KurimisettyTarun <169027569+KurimisettyTarun@users.noreply.github.com>
2026-03-31 15:08:08 +0530

    Time: 1 ms (94.77%), Space: 85 MB (86.38%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -2,17 +2,7 @@ class Solution {
     public boolean isMonotonic(int[] nums) {
         int i=0;
         int len  = nums.length;
-        int temp =0;
-        while(true)
-        {
-            if(i>=len-1)return true;
-            if(nums[i]!=nums[i+1] )
-            {
-                temp=nums[i]-nums[i+1];
-                break;
-            }
-            i++;
-        }
+        int temp =nums[0]-nums[len-1];
         if(temp>0)
         {
             for(int j=0;j<len-1;j++)
