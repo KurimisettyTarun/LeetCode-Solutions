111509bf295e39715a84d9f2b3cd8e696674581d
KurimisettyTarun <169027569+KurimisettyTarun@users.noreply.github.com>
2026-04-02 18:25:40 +0530

    Time: 37 ms (46.24%), Space: 47.2 MB (12.04%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.java
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.java
@@ -1,27 +1,28 @@
 class Solution {
     public int similarPairs(String[] words) {
-        int len= words.length;
-        int res =0;
+        int len = words.length;
+        int res = 0;
 
-        for(int i=0;i<len;i++)
-        {
-            Set<Character> fWord = new HashSet<>();
-            for(char c :words[i].toCharArray())
-            {
-                fWord.add(c);
+        List<Set<Character>> list = new ArrayList<>();
+
+        // Precompute sets
+        for (String word : words) {
+            Set<Character> set = new HashSet<>();
+            for (char c : word.toCharArray()) {
+                set.add(c);
             }
-            for(int j=i+1;j<len;j++)
-            {
-                Set<Character> sWord = new HashSet<>();
-                for(char c:words[j].toCharArray())
-                {
-                    sWord.add(c);
-                }
-                if(fWord.equals(sWord)){
+            list.add(set);
+        }
+
+        // Compare
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (list.get(i).equals(list.get(j))) {
                     res++;
                 }
             }
         }
+
         return res;
     }
 }
