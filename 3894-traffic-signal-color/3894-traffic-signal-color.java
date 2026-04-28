4c7fef7a3403c367e715d29d066f2c18e48e7058
KurimisettyTarun <169027569+KurimisettyTarun@users.noreply.github.com>
2026-04-28 09:07:01 +0530

    Time: 1 ms (71.03%), Space: 43.6 MB (19.94%) - LeetHub

--- 3894-traffic-signal-color/3894-traffic-signal-color.java
+++ 3894-traffic-signal-color/3894-traffic-signal-color.java
@@ -2,6 +2,6 @@ class Solution {
     public String trafficSignal(int timer) {
         
 
-        return timer==0?"Green":(timer==30?"Orange":(timer>30 && timer <=90)?"Red":"Invalid");
+        return timer==0?"Green":(timer==30?"Orange":(30<timer && timer <=90)?"Red":"Invalid");
     }
 }
