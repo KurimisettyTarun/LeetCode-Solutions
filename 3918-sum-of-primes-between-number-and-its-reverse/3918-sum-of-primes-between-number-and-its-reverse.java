a8c600724c412b0df4d2ba90c769e7ef86aa4f9e
KurimisettyTarun <169027569+KurimisettyTarun@users.noreply.github.com>
2026-05-03 09:36:22 +0530

    Time: 44 ms (7.58%), Space: 43 MB (19.5%) - LeetHub

--- 3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.java
+++ 3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.java
@@ -19,7 +19,7 @@ class Solution {
     public int primeOrZero(int n) {
         if (n <= 1) return 0;
 
-        for (int i = 2; i * i <= n; i++) {
+        for (int i = 2; i<= n/2; i++) {
             if (n % i == 0) {
                 return 0;
             }
