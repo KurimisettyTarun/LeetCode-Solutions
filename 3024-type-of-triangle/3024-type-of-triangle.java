a6db77917815bf6aa225ba0543013a38ac1acd25
KurimisettyTarun <169027569+KurimisettyTarun@users.noreply.github.com>
2026-05-08 09:00:15 +0530

    Time: 1 ms (58.4%), Space: 44 MB (9.66%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.java
+++ 3024-type-of-triangle/3024-type-of-triangle.java
@@ -4,9 +4,7 @@ class Solution {
         return "none";
         int temp = nums[0];
         if(nums[0]==temp && nums[1]==temp && nums[2]==temp)return "equilateral";
-        else if((nums[0]==nums[1] || nums[0]==nums[2])||
-                (nums[1]==nums[0] || nums[1]==nums[2])||
-                (nums[2]==nums[1] || nums[2]==nums[0])) return "isosceles";
+        else if(nums[0]==nums[1]||nums[1]==nums[2]||nums[2]==nums[0]) return "isosceles";
         return "scalene";
     }
 }
