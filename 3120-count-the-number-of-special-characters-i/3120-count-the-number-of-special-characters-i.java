ff238ad6b75824f66599fa0d4e90170b5a567049
KurimisettyTarun <169027569+KurimisettyTarun@users.noreply.github.com>
2026-05-26 10:25:29 +0530

    Time: 3 ms (31.81%), Space: 44.2 MB (16%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -1,36 +1,36 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
 
-        // Set<Character> set = new HashSet<>();
-        // for(char c : word.toCharArray())
-        // set.add(c);
+        Set<Character> set = new HashSet<>();
+        for(char c : word.toCharArray())
+        set.add(c);
 
-        // int cnt = 0;
-        // for(char i : word.toCharArray())
-        // {
-        //     char up =Character.toUpperCase(i);
-        //     char low = Character.toLowerCase(i);
-        //     if(set.contains(up) && set.contains(low)){
-        //         set.remove(up);
-        //         set.remove(low);
-        //         cnt++;
-        //     }
-        // }
-        // return cnt;
-
-        int cnt=0;
-       while(!word.isEmpty())
-       {
-            String x = String.valueOf(word.charAt(0));
-            if(word.contains(x.toLowerCase())&& word.contains(x.toUpperCase()))
-            {
+        int cnt = 0;
+        for(char i : word.toCharArray())
+        {
+            char up =Character.toUpperCase(i);
+            char low = Character.toLowerCase(i);
+            if(set.contains(up) && set.contains(low)){
+                set.remove(up);
+                set.remove(low);
                 cnt++;
-               word = word.replace(x.toLowerCase(),"");
-               word = word.replace(x.toUpperCase(),"");
             }
-            else
-            word = word.replace(x,"");
-       }
-       return cnt;
+        }
+        return cnt;
+
+        // int cnt=0;
+    //    while(!word.isEmpty())
+    //    {
+    //         String x = String.valueOf(word.charAt(0));
+    //         if(word.contains(x.toLowerCase())&& word.contains(x.toUpperCase()))
+    //         {
+    //             cnt++;
+    //            word = word.replace(x.toLowerCase(),"");
+    //            word = word.replace(x.toUpperCase(),"");
+    //         }
+    //         else
+    //         word = word.replace(x,"");
+    //    }
+    //    return cnt;
     }
 }
