4f52e9b67321acd3ae7503cb8a7a5e918a6a2af7
KurimisettyTarun <169027569+KurimisettyTarun@users.noreply.github.com>
2026-07-08 15:35:42 +0530

    Time: 2 ms (64.21%), Space: 44.4 MB (28.14%) - LeetHub

--- 3407-substring-matching-pattern/3407-substring-matching-pattern.java
+++ 3407-substring-matching-pattern/3407-substring-matching-pattern.java
@@ -20,16 +20,6 @@ class Solution {
         String s2 = s.substring(s.indexOf(p1)+p1.length());
 
         return s1.contains(p1)&&s2.contains(p2);
-        // String p1 = p.substring(0, p.indexOf('*'));
-        // String p2 = p.substring(p.indexOf('*') + 1);
-
-        // int first = s.indexOf(p1);
-
-        // if (first == -1)
-        //     return false;
-
-        // int second = s.indexOf(p2, first + p1.length());
-
-        // return second != -1;
+        
     }
 }
