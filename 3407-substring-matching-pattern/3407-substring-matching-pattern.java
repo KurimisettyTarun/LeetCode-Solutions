d0b5369206a4ca720c8abd294d714a1ba3005299
KurimisettyTarun <169027569+KurimisettyTarun@users.noreply.github.com>
2026-07-08 15:35:11 +0530

    Time: 2 ms (64.21%), Space: 44.4 MB (43.17%) - LeetHub

--- 3407-substring-matching-pattern/3407-substring-matching-pattern.java
+++ 3407-substring-matching-pattern/3407-substring-matching-pattern.java
@@ -1,39 +1,35 @@
 class Solution {
     public boolean hasMatch(String s, String p) {
-        // if(p.charAt(0)=='*')
-        // {
-        //     p=p.substring(1);
-        //     return s.contains(p);
-        // }
-        //  if(p.charAt(p.length()-1)=='*')
-        // {
-        //     p=p.substring(0,p.length()-1);
-        //     return s.contains(p);
-        // }
-        // String p1 = p.substring(0,p.indexOf("*"));
-        // String p2 = p.substring(p.indexOf("*")+1);
-        // System.out.println(p1);
-        // System.out.println(p2);
+        if(p.charAt(0)=='*')
+        {
+            p=p.substring(1);
+            return s.contains(p);
+        }
+         if(p.charAt(p.length()-1)=='*')
+        {
+            p=p.substring(0,p.length()-1);
+            return s.contains(p);
+        }
+        String p1 = p.substring(0,p.indexOf("*"));
+        String p2 = p.substring(p.indexOf("*")+1);
+        
+        if(!s.contains(p1))return false;
 
-        // String s1 = s.contains(p1)?s.substring(0,s.indexOf(p1)+p1.length()):null;
-        // if(s1==null)return false;
-        // System.out.println(s1);
+        String s1 =s.substring(0,s.indexOf(p1)+p1.length());
 
-        // String s2 = s.contains(p2)?s.substring(s.lastIndexOf(p1)):null;
-        // if(s2==null)return false;
-        // System.out.println(s2);
-        // if(s.indexOf(p1)==s.lastIndexOf(p2))return false;
-        // return s1.contains(p1)&&s2.contains(p2);
-        String p1 = p.substring(0, p.indexOf('*'));
-        String p2 = p.substring(p.indexOf('*') + 1);
+        String s2 = s.substring(s.indexOf(p1)+p1.length());
 
-        int first = s.indexOf(p1);
+        return s1.contains(p1)&&s2.contains(p2);
+        // String p1 = p.substring(0, p.indexOf('*'));
+        // String p2 = p.substring(p.indexOf('*') + 1);
 
-        if (first == -1)
-            return false;
+        // int first = s.indexOf(p1);
 
-        int second = s.indexOf(p2, first + p1.length());
+        // if (first == -1)
+        //     return false;
 
-        return second != -1;
+        // int second = s.indexOf(p2, first + p1.length());
+
+        // return second != -1;
     }
 }
